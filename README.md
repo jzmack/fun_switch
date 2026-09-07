31fe836ba0dda6c32b9e4de3ede2f8a8d116ab10
Jake Mackin <canesfanjake1@gmail.com>
2026-09-07 17:25:22 -0400

    fixed README and load_environment function

--- README.md
+++ README.md
@@ -12,12 +12,12 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 ```
-Create .env file
-
+Create .env file. It should be structured as so:
+```
 SWITCH_IP=<ip_address>
 SWITCH_USER=<username>
 SWITCH_PASSWORD=<password>
-
+```
 # Resources
 
 HPE API documentation:
--- load_environment.py
+++ load_environment.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-def load_environment(path_to_env=".env") -> tuple[str, str]:
+def load_environment(path_to_env=".env") -> tuple[str, str, str]:
     load_dotenv()
     API_USERNAME = os.getenv("SWITCH_USER") 
     API_PASSWORD = os.getenv("SWITCH_PASSWORD")
