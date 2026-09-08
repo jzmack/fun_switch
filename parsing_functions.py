c1dbab652343a445e3bdecb245cf93074d2d540b
Jake Mackin <canesfanjake1@gmail.com>
2026-09-08 14:52:10 -0400

    updated parsing_interface_data function to return a list of dictionaries containing the data I want from each interface

--- parsing_functions.py
+++ parsing_functions.py
@@ -7,15 +7,25 @@ def parse_bytes(bytes:int) -> tuple[int, int, int]:
     bytes_gb = bytes_mb / 1024
     return bytes_kb, bytes_mb, bytes_gb
 
-def parse_interface_data(interface_json:dict):
+def parse_interface_data(interface_json:dict) -> list[dict]:
+    all_int_stats = [] # list of dicts
     for key, value in interface_json.items():
+        stats_dict = {}
         if value["description"] == None:
             continue
         elif value["statistics"] == {}:
             continue
+        elif value["rate_statistics"] == {}:
+            continue
         else:
-            tx_bytes = value["statistics"]["tx_bytes"]
-            rx_bytes = value["statistics"]["rx_bytes"]
+            tx_bytes:int = value["statistics"]["tx_bytes"]
+            rx_bytes:int = value["statistics"]["rx_bytes"]
+            total_Bps:float = value["rate_statistics"]["bytes_per_second"]
+            stats_dict["interface"] = key
+            stats_dict["tx_bytes"] = round(tx_bytes,2)
+            stats_dict["rx_bytes"] = round(rx_bytes,2)
+            stats_dict["total_Bps"] = round(total_Bps,2)
+            # will add tx_Bps and rx_Bps in the future
             tx_bytes_kb, tx_bytes_mb, tx_bytes_gb = parse_bytes(tx_bytes)
             rx_bytes_kb, rx_bytes_mb, rx_bytes_gb = parse_bytes(rx_bytes)
             print(f"\n{key}: {value['description']}")
@@ -30,8 +40,12 @@ def parse_interface_data(interface_json:dict):
             print(f"Received GB: {rx_bytes_gb:.2f}")
 
         if "utilization" not in value["rate_statistics"]:
-           print(f"Utilization is 0%") 
+           print(f"Utilization is 0%")
+           stats_dict["util_pct"] = 0
         else:
-            utilization = value["rate_statistics"]["utilization"]
-            print(f"Utilization: {utilization:.2f}%")
-    return
+            util_pct:float = value["rate_statistics"]["utilization"]
+            stats_dict["util_pct"] = round(util_pct, 2) 
+            print(f"Utilization: {util_pct:.2f}%")
+        all_int_stats.append(stats_dict) 
+    print(all_int_stats)
+    return all_int_stats
---
+++ sql_functions.py
@@ -0,0 +1,5 @@
+def create_insert_statements(all_int_stats:list[dict]) -> list[str]:
+    pass
+
+def insert_to_db(insert_list:list[str]):
+    pass
