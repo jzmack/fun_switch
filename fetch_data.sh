fed1253e8a97d7d082574f6568d07a720f6957c5
Jake Mackin <canesfanjake1@gmail.com>
2026-09-08 22:22:14 -0400

    helps if you change to the right directory

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+cd /home/jzm/workspace/fun_switch
 source /home/jzm/workspace/fun_switch/.venv/bin/activate
 /home/jzm/workspace/fun_switch/main.py
-sleep 2
-deactivate
